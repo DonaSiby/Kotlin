378739ba37d0625172d3b3216d810686c8628165
Dona Siby <76901510+DonaSiby@users.noreply.github.com>
2021-06-01 20:07:23 +0530

    Create border.kt

---
+++ border.kt
@@ -0,0 +1,15 @@
+fun main() {
+    printBorder()
+    println("Happy Birthday, Dawn!")
+    printBorder()
+}
+
+//In the printBorder() method, use a repeat() statement to print the equal sign 23 times.
+//Instead of using println(), use print(), so that you do not jump to a new line after printing each "=".
+
+fun printBorder() {
+    repeat(23) {
+        print("=")
+    }
+    println()
+}
