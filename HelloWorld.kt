a748494b33dbaabc8261ef0b95ca81995443342a
Dona Siby <76901510+DonaSiby@users.noreply.github.com>
2021-06-05 19:11:59 +0530

    Update HelloWorld.kt

--- HelloWorld.kt
+++ HelloWorld.kt
@@ -1,10 +1,13 @@
-/*************************************************************************************************
+/*****************************************
 Author:Dona Siby
 Title :Hello world.
 Date  :01/06/2021
-*************************************************************************************************/
-//fun is a word in the Kotlin programming language. fun stands for function. A function is a section of a program that performs a specific task.
+******************************************/
+
+//fun is a word in the Kotlin programming language. fun stands for function.
+//A function is a section of a program that performs a specific task.
 //println tells the system to print a line of text.
+
 fun main() {
     println("Hello, world!")
 }
