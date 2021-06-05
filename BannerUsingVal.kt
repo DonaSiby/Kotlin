3d0c063f94368f94a1e3d06bdf82e8015d55231f
Dona Siby <76901510+DonaSiby@users.noreply.github.com>
2021-06-05 19:05:54 +0530

    Update BannerUsingVal.kt

--- BannerUsingVal.kt
+++ BannerUsingVal.kt
@@ -1,3 +1,8 @@
+/**************************************
+Author:Dona Siby
+Title :To create a border.
+Date  :03/06/2021
+***************************************/
 fun main() {
     val border = "%"
     printBorder(border)
